5297e72944dc9bf69cc0930285a54c7fc84dd65c
Vaskeptik <103656823+Vaskeptik@users.noreply.github.com>
2022-06-11 22:51:37 +0300

    end

--- js/main.js
+++ js/main.js
@@ -2,31 +2,7 @@ $(document).ready(function () {
 	$('.slider').slick({
 		arrows: true,
 		dots: true,
-		adaptiveHeight: true,
 		slidesToShow: 3,
-		// slidesToScroll: 3,
-		speed: 300,
-		easing: 'ease',
-		infinity: true,
-		// initialSlide: 3,
-		// autoplay: true,
-		// autoplaySpeed: 3000,
-		// pauseOnFocus: true,
-		// pauseOnHover: true,
-		// pauseOnDotsHover: true,
-		// pauseOnDotsHover: true,
-		// draggable: true,
-		// swipe:true,
-		// touchMove:true,
-		// touchThreshhold: 5,
-		waitForAnimate: true,
-		// centerMode: true,
-		// variableWidth: true,
-		// rows: 1,
-		// slidesPerRow: 3,
-		// vertical: true,
-		// verticalSwiping: true,
-		asNavFor: ".sliderBig",
 		responsive: [
 			{
 				breakpoint: 1280,
@@ -41,16 +17,69 @@ $(document).ready(function () {
 					// centerMode: true,
 				}
 			}
-		],
-		// mobileFirst: true,
-		// appendArrows: ('.wrapper'),
-		// appendDots: ('.wrapper'),
+		]
 	});
-	$('.sliderBig').slick({
-		arrows: false,
-		dots: false,
-		fade: true,
-		asNavFor: ".slider",
+
+	// Методы Слик
+
+	// $('.slider').on('afterChange', function (event, slick, currenSlide, nextSlide) {
+	// 	console.log(currenSlide);
+	// }
+	// );
+	// метод для корректной роботы
+	$('.slider').slick('setPosition');
+	// ----------
+	// $('.slider').slick('goTo', 3);
+	// ----------
+	$('.slider').slick('slickPrev', 3);
+	$('.slider').slick('slickNext', 3);
+	// ----------
+	$('.link-l').click(function (event) {
+		$('.slider').slick('slickprev', 3);
+	})
+	$('.link-r').click(function (event) {
+		$('.slider').slick('slickNext', 3);
+	})
+	// ----------
+	$('.link-play').click(function (event) {
+		$('.slider').slick('slickPlay');
+	})
+	$('.link-pause').click(function (event) {
+		$('.slider').slick('slickPause');
+	})
+	// ----------
+	$('.link-add').click(function (event) {
+		$('.slider').slick('slickAdd', '<div class="newslide">123</div>');
+		return false;
+	})
+	$('.link-remove').click(function (event) {
+		$('.slider').slick('slickRemove', 0);
+		return false;
+	})
+	// ----------
+	var filtered = false;
+	$('.img-filter').on('click', function () {
+		if (filtered === false) {
+			$('.slider').slick('slickFilter', '.filter');
+			$(this).text('убрать фильтр');
+			filtered = true;
+		} else {
+			$('.slider').slick('slickUnfilter');
+			$(this).text('добавить фильтр');
+			filtered = false;
+		};
+		return false;
 	});
+
+	// ----------
+	$('.slider').on('afterChange', function () {
+		var s = $('.slider').slick('slickGetOption', 'slidesToShow');
+		console.log(s);
+	});
+	// ----------
+	// $('.slider').slick('slickSetOption', 'slidesToShow', 2);
+	// ----------
+	// $('.slider').slick('unslick');
+
 });
 
