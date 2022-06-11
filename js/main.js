88998f565842a0079b84ff642f7260567a3376bf
Vaskeptik <103656823+Vaskeptik@users.noreply.github.com>
2022-06-11 20:15:56 +0300

    Center mode

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 $(document).ready(function () {
-	$('.slider').slick();
+	$('.slider').slick({
+		arrows: true,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 3,
+		// slidesToScroll: 3,
+		speed: 300,
+		easing: 'ease',
+		infinity: true,
+		// initialSlide: 3,
+		// autoplay: true,
+		// autoplaySpeed: 3000,
+		// pauseOnFocus: true,
+		// pauseOnHover: true,
+		// pauseOnDotsHover: true,
+		// pauseOnDotsHover: true,
+		// draggable: true,
+		// swipe:true,
+		// touchMove:true,
+		touchThreshhold: 5,
+		waitForAnimate: true,
+		centerMode: true,
+
+
+	});
 });
 
